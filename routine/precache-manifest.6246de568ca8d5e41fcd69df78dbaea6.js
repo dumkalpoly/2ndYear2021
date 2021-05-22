0e877bf5ca9096333ba25bc7a2088bcb7c4ef070
dumkalpoly <dumkalpoly@example.com>
2021-05-22 21:23:12 +0530

    Another schedule

---
+++ routine/precache-manifest.6246de568ca8d5e41fcd69df78dbaea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a17882e26a1bed7b3dd2accd587ce1",
+    "url": "/2ndYear2021/routine/index.html"
+  },
+  {
+    "revision": "175a84d23fd162bd974c",
+    "url": "/2ndYear2021/routine/static/css/main.86e77ec8.chunk.css"
+  },
+  {
+    "revision": "2a4a468d549b13e61154",
+    "url": "/2ndYear2021/routine/static/js/2.31dc192d.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/2ndYear2021/routine/static/js/2.31dc192d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175a84d23fd162bd974c",
+    "url": "/2ndYear2021/routine/static/js/main.db2f8563.chunk.js"
+  },
+  {
+    "revision": "d7d36c3c5520a9e064db",
+    "url": "/2ndYear2021/routine/static/js/runtime-main.adac0186.js"
+  }
+]);
